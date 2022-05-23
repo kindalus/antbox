fa7bf5db4fc094bf15484a894ae751ca561553e0
Dzerjinski Lemos <dzerjinski@yahoo.com>
2022-05-23 10:14:32 +0100

    Seg 23 Mai 2022 10:14:32 WAT

--- src/ecm/builtin_aspects/web_content.ts
+++ src/ecm/builtin_aspects/web_content.ts
@@ -1,15 +1,18 @@
 export interface WebContent {
-	pt: string;
-	en?: string;
-	es?: string;
-	fr?: string;
+  uuid: string;
+  fid: string;
+  title: string;
+  pt: string;
+  en?: string;
+  es?: string;
+  fr?: string;
 }
 
 export default {
-	uuid: "web-content",
-	title: "Conteúdo Web",
-	builtIn: true,
-	description: "Representa um conteúdo Web",
-	mimetypeConstraints: ["application/json"],
-	properties: [],
+  uuid: "web-content",
+  title: "Conteúdo Web",
+  builtIn: true,
+  description: "Representa um conteúdo Web",
+  mimetypeConstraints: ["application/json"],
+  properties: [],
 };

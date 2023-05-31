1787619ff438664910c59ed6cf24c10e0999245d
Dzerjinski Lemos <dzerjinski@yahoo.com>
2023-05-31 11:18:54 +0100

    Wed May 31 11:18:54 WAT 2023

--- src/application/builtin_actions/delete_all.ts
+++ src/application/builtin_actions/delete_all.ts
@@ -1,39 +1,41 @@
 import { Action } from "../../domain/actions/action.ts";
 import { RunContext } from "../../domain/actions/run_context.ts";
 import { NodeNotFoundError } from "../../domain/nodes/node_not_found_error.ts";
+import { AntboxError } from "../../shared/antbox_error.ts";
 import { Either } from "../../shared/either.ts";
 
 export default {
-  uuid: "delete_all",
-  title: "Eliminar",
-  description: "Elimina todos os nós selecionados",
-  builtIn: true,
-  multiple: false,
-  filters: [],
-  params: [],
-  runManually: true,
-  runOnCreates: false,
-  runOnUpdates: false,
+	uuid: "delete_all",
+	title: "Eliminar",
+	description: "Elimina todos os nós selecionados",
+	builtIn: true,
+	multiple: false,
+	filters: [],
+	params: [],
+	runManually: true,
+	runOnCreates: false,
+	runOnUpdates: false,
 
-  async run(
-    ctx: RunContext,
-    uuids: string[],
-    _params?: Record<string, unknown>
-  ): Promise<void | Error> {
-    const tasks = uuids.map((uuid) => ctx.nodeService.delete(uuid));
+	async run(
+		ctx: RunContext,
+		uuids: string[],
+		_params?: Record<string, unknown>,
+	): Promise<void | Error> {
+		const tasks = uuids.map((uuid) => ctx.nodeService.delete(uuid));
 
-    const results = await Promise.all(tasks);
+		const results = await Promise.all(tasks);
 
-    const errors = results.filter(errorResultsOnly);
+		const errors = results.filter(errorResultsOnly);
 
-    if (errors.length > 0) {
-      return errors[0].value;
-    }
+		if (errors.length > 0) {
+			errors.forEach((e) => console.error((e.value as AntboxError).message));
+			return errors[0].value;
+		}
 
-    return;
-  },
+		return;
+	},
 } as Action;
 
 function errorResultsOnly(voidOrErr: Either<NodeNotFoundError, void>): boolean {
-  return voidOrErr.isLeft();
+	return voidOrErr.isLeft();
 }

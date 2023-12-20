839e68597a29365c636c15dcee5acddbe0826199
Dzerjinski Lemos <dzerjinski@yahoo.com>
2023-12-20 23:27:25 +0100

    add trupakali changes, ignoring non important files

--- src/adapters/oak/get_tenant.ts
+++ src/adapters/oak/get_tenant.ts
@@ -1,8 +1,22 @@
 import { Context } from "../../../deps.ts";
 import { AntboxTenant } from "./setup_oak_server.ts";
 
-export function getTenant(ctx: Context, tenants: AntboxTenant[]) {
-  const tenantName = ctx.request.headers.get("x-tenant") ?? tenants[0].name;
+export function getTenantByHeaders(ctx: Context, tenants: AntboxTenant[]) {
+	const tenant = ctx.request.headers.get("x-tenant") ?? tenants[0].name;
 
-  return tenants.find((t) => t.name === tenantName) ?? tenants[0];
+	return tenants.find((t) => t.name === tenant);
+}
+
+export function getTenantBySearchParams(ctx: Context, tenants: AntboxTenant[]) {
+	const tenantName = ctx.request.url.searchParams.get("x-tenant") || "";
+
+	return tenants.find((tenant) => tenant.name === tenantName);
+}
+
+export function getTenant(ctx: Context, tenants: AntboxTenant[]) {
+	const tenant = getTenantBySearchParams(ctx, tenants) ??
+		getTenantByHeaders(ctx, tenants) ??
+		tenants[0];
+
+	return tenant;
 }

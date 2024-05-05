faaabf1df6cee7b36443bc99c1d6920ec29e71a6
Dzerjinski Lemos <dzerjinski@yahoo.com>
2024-05-05 04:18:08 +0100

    feat: add null providers

---
+++ src/adapters/null/null_storage_provider.ts
@@ -0,0 +1,31 @@
+import { NodeNotFoundError } from "../../domain/nodes/node_not_found_error.ts";
+import { StorageProvider, WriteFileOpts } from "../../domain/providers/storage_provider.ts";
+import { AntboxError } from "../../shared/antbox_error.ts";
+import { Either, left, right } from "../../shared/either.ts";
+import { Event } from "../../shared/event.ts";
+import { EventHandler } from "../../shared/event_handler.ts";
+
+export class NullStorageProvider implements StorageProvider {
+	delete(_uuid: string): Promise<Either<AntboxError, void>> {
+		return Promise.resolve(right(undefined));
+	}
+
+	write(
+		_uuid: string,
+		_file: File,
+		_opts?: WriteFileOpts | undefined,
+	): Promise<Either<AntboxError, void>> {
+		return Promise.resolve(right(undefined));
+	}
+
+	read(uuid: string): Promise<Either<AntboxError, File>> {
+		return Promise.resolve(left(new NodeNotFoundError(uuid)));
+	}
+
+	startListeners(_bus: (eventId: string, handler: EventHandler<Event>) => void): void {
+	}
+}
+
+export default function buildNullStorageProvider(): Promise<Either<AntboxError, StorageProvider>> {
+	return Promise.resolve(right(new NullStorageProvider()));
+}

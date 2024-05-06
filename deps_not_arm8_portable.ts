f36b4a950f0500e6734e0503bdd6f4b45f546e07
Dzerjinski Lemos <dzerjinski@yahoo.com>
2024-05-06 13:54:43 +0100

    Mon May  6 13:54:43 WAT 2024

--- deps_not_arm8_portable.ts
+++ deps_not_arm8_portable.ts
@@ -1,5 +1,4 @@
 import PouchDB from "npm:pouchdb-node@8.0.1";
 import PouchDbFind from "npm:pouchdb-find@8.0.1";
-import leveldown from "npm:leveldown@5.6.0";
 
-export { leveldown, PouchDB, PouchDbFind };
+export { PouchDB, PouchDbFind };

8fa973646e7e4c4a331ad7e4f928bbb9417b02f1
Dzerjinski Lemos <dzerjinski@yahoo.com>
2025-01-23 16:25:59 +0100

    qui 23 jan 2025 16:25:59 WAT

---
+++ src/adapters/h3/nodes_router.ts
@@ -0,0 +1,12 @@
+import { createRouter, Router } from "@h3";
+import { AntboxTenant } from "../../api/antbox_tenant.ts";
+import { listHandler } from "../../api/nodes_handlers.ts";
+import { adapt } from "./adapt.ts";
+
+export default function (tenants: AntboxTenant[]): Router {
+	const router = createRouter();
+
+	router.get("/", adapt(listHandler(tenants)));
+
+	return router;
+}
--- src/adapters/oak/nodes_router.ts
+++ src/adapters/oak/nodes_router.ts
@@ -1,14 +1,52 @@
-import { Context, getQuery, Router, Status } from "../../../deps.ts";
-import { Node } from "../../domain/nodes/node.ts";
-import { ContextWithParams } from "./context_with_params.ts";
-import { getRequestContext } from "./get_request_context.ts";
-import { getTenant } from "./get_tenant.ts";
-import { processEither } from "./process_either.ts";
-import { processError } from "./process_error.ts";
-import { sendBadRequest, sendOK } from "./send_response.ts";
+import { Router } from "../../../deps.ts";
 import { AntboxTenant } from "./setup_oak_server.ts";
 
-export default function (tenants: AntboxTenant[]) {
+import {
+  copyHandler,
+  createHandler,
+  deleteHandler,
+  duplicateHandler,
+  evaluateHandler,
+  exportHandler,
+  findHandler,
+  getHandler,
+  listHandler,
+  recognizeHandler,
+  updateHandler,
+} from "../../api/nodes_handlers.ts";
+import { adapt } from "./adapt.ts";
+
+export default function (tenants: AntboxTenant[]): Router {
+  const nodesRouter = new Router({ prefix: "/nodes" });
+
+  nodesRouter.get("/:uuid", adapt(getHandler(tenants)));
+  nodesRouter.get("/:uuid/-/export", adapt(exportHandler(tenants)));
+  nodesRouter.get("/:uuid/-/duplicate", adapt(duplicateHandler(tenants)));
+  nodesRouter.get("/:uuid/-/evaluate", adapt(evaluateHandler(tenants)));
+
+  nodesRouter.get("/", adapt(listHandler(tenants)));
+
+  nodesRouter.post("/", adapt(createHandler(tenants)));
+  nodesRouter.post("/:uuid/-/copy", adapt(copyHandler(tenants)));
+
+  nodesRouter.post("/-/query", adapt(findHandler(tenants)));
+  nodesRouter.post("/-/find", adapt(findHandler(tenants)));
+
+  nodesRouter.patch("/:uuid", adapt(updateHandler(tenants)));
+
+  nodesRouter.delete("/:uuid", adapt(deleteHandler(tenants)));
+
+  nodesRouter.get("/:uuid/-/ocr", adapt(recognizeHandler(tenants)));
+
+  return nodesRouter;
+}
+/*
+  const listHandler = async (ctx: Context) => {
+		const handler = nodesApi.listHandler(tenants);
+
+		const req = adapt(ctx);
+	};
+
 	const listHandler = (ctx: Context) => {
 		const service = getTenant(ctx, tenants).service;
 		const query = getQuery(ctx);
@@ -27,158 +65,137 @@ export default function (tenants: AntboxTenant[]) {
 			.catch((err) => processError(err, ctx));
 	};
 
-	const getHandler = (ctx: ContextWithParams) => {
-		const service = getTenant(ctx, tenants).service;
-		return service
-			.get(getRequestContext(ctx), ctx.params.uuid)
-			.then((result) => {
-				if (result.isLeft()) {
-					return processError(result.value, ctx);
-				}
+  const getHandler = (ctx: ContextWithParams) => {
+    const service = getTenant(ctx, tenants).service;
+    return service
+      .get(getRequestContext(ctx), ctx.params.uuid)
+      .then((result) => {
+        if (result.isLeft()) {
+          return processError(result.value, ctx);
+        }
 
-				ctx.response.status = Status.OK;
-				ctx.response.type = "json";
-				ctx.response.body = result.value;
-			})
-			.catch((err) => processError(err, ctx));
-	};
+        ctx.response.status = Status.OK;
+        ctx.response.type = "json";
+        ctx.response.body = result.value;
+      })
+      .catch((err) => processError(err, ctx));
+  };
 
-	const exportHandler = (ctx: ContextWithParams) => {
-		const service = getTenant(ctx, tenants).service;
-		const uuid = ctx.params.uuid;
-		const requestContext = getRequestContext(ctx);
+  const exportHandler = (ctx: ContextWithParams) => {
+    const service = getTenant(ctx, tenants).service;
+    const uuid = ctx.params.uuid;
+    const requestContext = getRequestContext(ctx);
 
-		return Promise.all([
-			service.get(requestContext, uuid),
-			service.export(requestContext, uuid),
-		])
-			.then(([node, blob]) => {
-				if (node.isLeft()) {
-					return processError(node.value, ctx);
-				}
+    return Promise.all([
+      service.get(requestContext, uuid),
+      service.export(requestContext, uuid),
+    ])
+      .then(([node, blob]) => {
+        if (node.isLeft()) {
+          return processError(node.value, ctx);
+        }
 
-				if (blob.isLeft()) {
-					return processError(blob.value, ctx);
-				}
+        if (blob.isLeft()) {
+          return processError(blob.value, ctx);
+        }
 
-				ctx.response.headers.set("Content-Type", mapSystemNodeType(node.value.mimetype));
-				ctx.response.headers.set("Content-length", blob.value.size.toString());
+        ctx.response.headers.set(
+          "Content-Type",
+          mapSystemNodeType(node.value.mimetype),
+        );
+        ctx.response.headers.set("Content-length", blob.value.size.toString());
 
-				ctx.response.type = "blob";
-				ctx.response.body = blob.value;
-			})
-			.catch((err) => processError(err, ctx));
-	};
+        ctx.response.type = "blob";
+        ctx.response.body = blob.value;
+      })
+      .catch((err) => processError(err, ctx));
+  };
 
-	const createHandler = async (ctx: Context) => {
-		const service = getTenant(ctx, tenants).service;
-		const metadata: Partial<Node> = await ctx.request.body().value;
+  const createHandler = async (ctx: Context) => {
+    const service = getTenant(ctx, tenants).service;
+    const metadata: Partial<Node> = await ctx.request.body().value;
 
-		if (!metadata?.mimetype) {
-			return Promise.resolve(sendBadRequest(ctx, "{ mimetype } not given"));
-		}
+    if (!metadata?.mimetype) {
+      return Promise.resolve(sendBadRequest(ctx, "{ mimetype } not given"));
+    }
 
-		return service
-			.create(getRequestContext(ctx), metadata)
-			.then((result) => processEither(ctx, result))
-			.catch((err) => processError(err, ctx));
-	};
+    return service
+      .create(getRequestContext(ctx), metadata)
+      .then((result) => processEither(ctx, result))
+      .catch((err) => processError(err, ctx));
+  };
 
-	const updateHandler = async (ctx: ContextWithParams) => {
-		const service = getTenant(ctx, tenants).service;
-		const body = await ctx.request.body().value;
+  const updateHandler = async (ctx: ContextWithParams) => {
+    const service = getTenant(ctx, tenants).service;
+    const body = await ctx.request.body().value;
 
-		return service
-			.update(getRequestContext(ctx), ctx.params.uuid, body)
-			.then((result) => processEither(ctx, result))
-			.catch((err) => processError(err, ctx));
-	};
+    return service
+      .update(getRequestContext(ctx), ctx.params.uuid, body)
+      .then((result) => processEither(ctx, result))
+      .catch((err) => processError(err, ctx));
+  };
 
-	const deleteHandler = (ctx: ContextWithParams) => {
-		const service = getTenant(ctx, tenants).service;
-		return service
-			.delete(getRequestContext(ctx), ctx.params.uuid)
-			.then((result) => processEither(ctx, result))
-			.catch((err) => processError(err, ctx));
-	};
+  const deleteHandler = (ctx: ContextWithParams) => {
+    const service = getTenant(ctx, tenants).service;
+    return service
+      .delete(getRequestContext(ctx), ctx.params.uuid)
+      .then((result) => processEither(ctx, result))
+      .catch((err) => processError(err, ctx));
+  };
 
-	const copyHandler = async (ctx: ContextWithParams) => {
-		const service = getTenant(ctx, tenants).service;
-		const { to }: { to: string } = await ctx.request.body().value;
+  const copyHandler = async (ctx: ContextWithParams) => {
+    const service = getTenant(ctx, tenants).service;
+    const { to }: { to: string } = await ctx.request.body().value;
 
-		return service
-			.copy(getRequestContext(ctx), ctx.params.uuid, to)
-			.then((result) => processEither(ctx, result))
-			.catch((err) => processError(err, ctx));
-	};
+    return service
+      .copy(getRequestContext(ctx), ctx.params.uuid, to)
+      .then((result) => processEither(ctx, result))
+      .catch((err) => processError(err, ctx));
+  };
 
-	const duplicateHandler = (ctx: ContextWithParams) => {
-		const service = getTenant(ctx, tenants).service;
-		return service
-			.duplicate(getRequestContext(ctx), ctx.params.uuid)
-			.then((result) => processEither(ctx, result))
-			.catch((err) => processError(err, ctx));
-	};
+  const duplicateHandler = (ctx: ContextWithParams) => {
+    const service = getTenant(ctx, tenants).service;
+    return service
+      .duplicate(getRequestContext(ctx), ctx.params.uuid)
+      .then((result) => processEither(ctx, result))
+      .catch((err) => processError(err, ctx));
+  };
 
-	const findHandler = async (ctx: Context) => {
-		const service = getTenant(ctx, tenants).service;
-		const { filters, pageSize, pageToken } = await ctx.request.body().value;
+  const findHandler = async (ctx: Context) => {
+    const service = getTenant(ctx, tenants).service;
+    const { filters, pageSize, pageToken } = await ctx.request.body().value;
 
-		return service
-			.find(getRequestContext(ctx), filters, pageSize, pageToken)
-			.then((result) => processEither(ctx, result))
-			.catch((err) => processError(err, ctx));
-	};
+    return service
+      .find(getRequestContext(ctx), filters, pageSize, pageToken)
+      .then((result) => processEither(ctx, result))
+      .catch((err) => processError(err, ctx));
+  };
 
-	const evaluateHandler = (ctx: ContextWithParams) => {
-		const service = getTenant(ctx, tenants).service;
-		return service
-			.evaluate(getRequestContext(ctx), ctx.params.uuid)
-			.then((result) => processEither(ctx, result))
-			.catch((err) => processError(err, ctx));
-	};
+  const evaluateHandler = (ctx: ContextWithParams) => {
+    const service = getTenant(ctx, tenants).service;
+    return service
+      .evaluate(getRequestContext(ctx), ctx.params.uuid)
+      .then((result) => processEither(ctx, result))
+      .catch((err) => processError(err, ctx));
+  };
 
-	const recognizeHandler = (ctx: ContextWithParams) => {
-		const service = getTenant(ctx, tenants).service;
+  const recognizeHandler = (ctx: ContextWithParams) => {
+    const service = getTenant(ctx, tenants).service;
 
-		return service
-			.recognizeText(getRequestContext(ctx), ctx.params.uuid)
-			.then((result) => processEither(ctx, result))
-			.catch((err) => processError(err, ctx));
-	};
-
-	const nodesRouter = new Router({ prefix: "/nodes" });
-
-	nodesRouter.get("/:uuid", getHandler);
-	nodesRouter.get("/:uuid/-/export", exportHandler);
-	nodesRouter.get("/:uuid/-/duplicate", duplicateHandler);
-	nodesRouter.get("/:uuid/-/evaluate", evaluateHandler);
-
-	nodesRouter.get("/", listHandler);
-
-	nodesRouter.post("/", createHandler);
-	nodesRouter.post("/:uuid/-/copy", copyHandler);
-
-	nodesRouter.post("/-/query", findHandler);
-	nodesRouter.post("/-/find", findHandler);
-
-	nodesRouter.patch("/:uuid", updateHandler);
-
-	nodesRouter.delete("/:uuid", deleteHandler);
-
-	nodesRouter.get("/:uuid/-/ocr", recognizeHandler);
-
-	return nodesRouter;
-}
+    return service
+      .recognizeText(getRequestContext(ctx), ctx.params.uuid)
+      .then((result) => processEither(ctx, result))
+      .catch((err) => processError(err, ctx));
+  };
 
 function mapSystemNodeType(type: string): string {
-	switch (type) {
-		case Node.ASPECT_MIMETYPE:
-			return "application/json";
-		case Node.EXT_MIMETYPE:
-		case Node.ACTION_MIMETYPE:
-			return "text/javascript";
-		default:
-			return type;
-	}
-}
+  switch (type) {
+    case Node.ASPECT_MIMETYPE:
+      return "application/json";
+    case Node.EXT_MIMETYPE:
+    case Node.ACTION_MIMETYPE:
+      return "text/javascript";
+    default:
+      return type;
+  }
+}*/

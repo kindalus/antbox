91c08abb7c77ddccffaa6222d438e5f3ddce1e19
silvano-paulino <silvanocavimbi20@gmail.com>
2025-02-20 09:49:38 +0100

    test: add convension of project

--- src/domain/exts/ext_node_test.ts
+++ src/domain/exts/ext_node_test.ts
@@ -4,7 +4,7 @@ import { Folders } from "../nodes/folders.ts";
 import { Nodes } from "../nodes/nodes.ts";
 import { ExtNode } from "./ext_node.ts";
 
-Deno.test("ExtNode constructor should initialize", () => {
+Deno.test("ExtNode.constructor should initialize", () => {
     const extNode = ExtNode.create({ title: "ExtNode", owner: "user@domain.com" })
     
     assertEquals(extNode.right.title, "ExtNode")
@@ -13,43 +13,34 @@ Deno.test("ExtNode constructor should initialize", () => {
     assertEquals(extNode.right.parent, Folders.EXT_FOLDER_UUID)
 })
 
-Deno.test("ExtNode constructor should throw error if owner is missing", () => {
+Deno.test("ExtNode.constructor should throw error if owner is missing", () => {
     const extNode = ExtNode.create({ title:"ExtNode" })
 
     assertEquals(extNode.isLeft(), true)
     assertEquals((extNode.value as ValidationError).message, "Node.owner is required")
 })
 
-Deno.test("ExtNode constructor should throw error if title is missing", () => {
+Deno.test("ExtNode.constructor should throw error if title is missing", () => {
     const extNode = ExtNode.create({ title:"", owner: "user@domain.com" })
 
     assertEquals(extNode.isLeft(), true)
     assertEquals((extNode.value as ValidationError).message, "Node.title is required")
 })
 
-Deno.test("ExtNode update should modify title", () => {
-    const extNode = ExtNode.create({title: "ExtNode title", owner:"user@domain.com"})
+Deno.test("ExtNode.update should modify title and description", () => {
+    const extNode = ExtNode.create({title: "ExtNode", owner:"user@domain.com"})
 
-    const result = extNode.right.update({ title: "ExtNode title custom" })
-
-    assertEquals(result.isRight(), true)
-    assertEquals(extNode.right.title, "ExtNode title custom")
-})
-
-Deno.test("ExtNode update should mopdify description", () => {
-    const extNode = ExtNode.create({ 
-        title: "ExtNode", 
-        owner:"user@domain.com", 
-        description: "Ext complete"
+    const result = extNode.right.update({ 
+        title: "New title", 
+        description: "New description" 
     })
 
-    const result = extNode.right.update({ description: "Ext complete successfully" })
-    
     assertEquals(result.isRight(), true)
-    assertEquals(extNode.right.description, "Ext complete successfully")
+    assertEquals(extNode.right.title, "New title")
+    assertEquals(extNode.right.description, "New description")
 })
 
-Deno.test("ExtNode update should not modify parent", () => {
+Deno.test("ExtNode.update should not modify parent", () => {
     const extNode = ExtNode.create({ title:"ExtNode", owner:"user@domain.com" })
 
     const result = extNode.right.update({ parent:"--root--" })
@@ -59,8 +50,8 @@ Deno.test("ExtNode update should not modify parent", () => {
     assertEquals(result.value.errors[0].message, "Invalid ExtNode Parent: --root--")
 })
 
-Deno.test("ExtNode update should not modify mimetype", () => {
-    const extNode = ExtNode.create({ title: "ExNode", owner: "user@gmail.com" })
+Deno.test("ExtNode.update should not modify mimetype", () => {
+    const extNode = ExtNode.create({ title: "ExtNode", owner: "user@gmail.com" })
 
     extNode.right.update({ mimetype: "image/jpg" }) 
     assertEquals(extNode.right.mimetype, Nodes.EXT_MIMETYPE)

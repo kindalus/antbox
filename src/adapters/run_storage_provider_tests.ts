8541900dd0458b29b53b4747566345cdbdd2fb0b
Mário Varela <trupakufi@gmail.com>
2025-02-27 10:15:08 +0100

    style: prettify

--- src/adapters/run_storage_provider_tests.ts
+++ src/adapters/run_storage_provider_tests.ts
@@ -6,26 +6,25 @@ import { UuidGenerator } from "../shared/uuid_generator.ts";
 import { NodeFileNotFoundError } from "../domain/nodes/node_file_not_found_error.ts";
 
 if (!process.env.NODE_ENV && process.argv.length < 3) {
-    console.error("This script must be run with command line arguments.");
-    process.exit(1);
+  console.error("This script must be run with command line arguments.");
+  process.exit(1);
 }
 
 if (!process.env.NODE_ENV) {
-    await Bun.spawn({
-        cmd: [
-            "/home/spaulino/.bun/bin/bun",
-            //process.argv0,
-            "test",
-            // "--inspect-wait",
-            "./src/adapters/run_storage_provider_tests.ts",
-          ],
-        env: { TEST_PARAMS: process.argv.slice(2).join(";") },
-    }).exited;
+  await Bun.spawn({
+    cmd: [
+      process.argv0,
+      "test",
+      // "--inspect-wait",
+      "./src/adapters/run_storage_provider_tests.ts",
+    ],
+    env: { TEST_PARAMS: process.argv.slice(2).join(";") },
+  }).exited;
 
-    process.exit(0)
+  process.exit(0);
 }
 
-let storage: StorageProvider
+let storage: StorageProvider;
 
 beforeAll(async () => {
   const args = process.env.TEST_PARAMS?.split(";");
@@ -42,41 +41,49 @@ beforeAll(async () => {
 });
 
 describe("write", () => {
-    test("should write",  async () => {
-        const uuid = UuidGenerator.generate()
-        const file = new File(["Something"], "some.txt", {type: "text/plain"})
-        const writeResult = await storage.write(uuid, file, {mimetype: file.type, parent: "--parent--", title: "Something"})
-        expect(writeResult.isRight()).toBeTruthy();
+  test("should write", async () => {
+    const uuid = UuidGenerator.generate();
+    const file = new File(["Something"], "some.txt", { type: "text/plain" });
+    const writeResult = await storage.write(uuid, file, {
+      mimetype: file.type,
+      parent: "--parent--",
+      title: "Something",
+    });
+    expect(writeResult.isRight()).toBeTruthy();
 
-        const readResult = await storage.read(uuid);
-        expect(readResult.isRight()).toBeTruthy()
+    const readResult = await storage.read(uuid);
+    expect(readResult.isRight()).toBeTruthy();
 
-        if(readResult.isRight()) {
-            const readFile = readResult.value
-            expect(readFile.size).toBe(file.size)
-            expect(readFile.type).toBe(file.type)
-        }
-    })
-})
+    if (readResult.isRight()) {
+      const readFile = readResult.value;
+      expect(readFile.size).toBe(file.size);
+      expect(readFile.type).toBe(file.type);
+    }
+  });
+});
 
 describe("delete", () => {
-    test("should delete",  async () => {
-        const uuid = UuidGenerator.generate()
-        const file = new File(["Something"], "some.txt", {type: "text/plain"})
-        const writeResult = await storage.write(uuid, file, {mimetype: file.type, parent: "--parent--", title: "Something"})
-        expect(writeResult.isRight()).toBeTruthy();
+  test("should delete", async () => {
+    const uuid = UuidGenerator.generate();
+    const file = new File(["Something"], "some.txt", { type: "text/plain" });
+    const writeResult = await storage.write(uuid, file, {
+      mimetype: file.type,
+      parent: "--parent--",
+      title: "Something",
+    });
+    expect(writeResult.isRight()).toBeTruthy();
 
-        const deleteResult = await storage.delete(uuid);
-        expect(deleteResult.isRight()).toBeTruthy()
+    const deleteResult = await storage.delete(uuid);
+    expect(deleteResult.isRight()).toBeTruthy();
 
-        const readResult = await storage.read(uuid);
-        expect(readResult.isLeft()).toBeTruthy()
-        expect(readResult.value).toBeInstanceOf(NodeFileNotFoundError)
-    })
+    const readResult = await storage.read(uuid);
+    expect(readResult.isLeft()).toBeTruthy();
+    expect(readResult.value).toBeInstanceOf(NodeFileNotFoundError);
+  });
 
-    test("should not delete",  async () => {
-        const deleteResult = await storage.delete("some");
-        expect(deleteResult.isLeft()).toBeTruthy()
-        expect(deleteResult.value).toBeInstanceOf(NodeFileNotFoundError)
-    })
-})
+  test("should not delete", async () => {
+    const deleteResult = await storage.delete("unkwown");
+    expect(deleteResult.isLeft()).toBeTruthy();
+    expect(deleteResult.value).toBeInstanceOf(NodeFileNotFoundError);
+  });
+});

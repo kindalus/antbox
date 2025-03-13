087b24a2bb937b6780e9ae956cf58053c782c54d
silvano-paulino <silvanocavimbi20@gmail.com>
2025-03-13 14:36:57 +0100

    refactor: rename domain errors user exists params

--- src/domain/users_groups/invalid_fullname_format_error.ts
+++ src/domain/users_groups/invalid_fullname_format_error.ts
@@ -3,10 +3,10 @@ import { AntboxError } from "shared/antbox_error.ts";
 export class InvalidFullNameFormatError extends AntboxError {
   static ERROR_CODE = "InvalidFullnameFormatError";
 
-  constructor(fullName: string) {
+  constructor(email: string) {
     super(
       InvalidFullNameFormatError.ERROR_CODE,
-      `Invalid Fullname Format: ${ fullName }`,
+      `Invalid Fullname Format: ${email}`,
     );
   }
 }
--- src/domain/users_groups/user_exists_error.ts
+++ src/domain/users_groups/user_exists_error.ts
@@ -3,10 +3,10 @@ import { AntboxError } from "shared/antbox_error.ts";
 export class UserExistsError extends AntboxError {
   static ERROR_CODE = "UserExistsError";
 
-  constructor(uuid: string) {
+  constructor(email: string) {
     super(
       UserExistsError.ERROR_CODE,
-      `User already exists for the uuid: ${ uuid }`,
+      `User already exists for the uuid: ${email}`,
     );
   }
 }
